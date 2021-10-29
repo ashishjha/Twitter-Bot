3925b80704f34c9d46fbfaf7e5bf1ca7a09faae5
ASHISH JHA <55405046+ashishjha@users.noreply.github.com>
2021-10-29 22:06:00 +0530

    Added new #tags

--- bot.js
+++ bot.js
@@ -87,6 +87,6 @@ function retweet(searchText) {
 // Run every 60 seconds
 setInterval(function () {
   retweet(
-    '#javascript OR #nodejs OR #datascience oR #mulesoft OR #salesforce OR #devops OR #python OR #machinelearning OR #aws OR #azure'
+    '#javascript OR #nodejs OR #datascience oR #mulesoft OR #salesforce OR #devops OR #python OR #machinelearning OR #aws OR #azure OR #MuleSoft OR #API OR #SalesForce'
   )
 }, 60000)
